1ebece2da4236172bb531412de10c0789fe0cd3f
Giorgia Nadizar <giorgia.nadizar@gmail.com>
2024-09-10 15:00:22 +0200

    conditional functions

--- src/libraries/integer/cond.jl
+++ src/libraries/integer/cond.jl
@@ -31,6 +31,35 @@ function is_eq_to(n::Number, to::Number, args...)
     return Int(n == to)
 end
 
+"""
+    experimental_is_gt(n::Number, then::Number, args...)
+
+Returns 1 if `n>then`, 0 otherwise.
+"""
+function experimental_is_gt(n::Number, then::Number, args...)
+    return Int(n > then)
+end
+
+"""
+    experimental_is_lt(n::Number, then::Number, args...)
+
+Returns 1 if `n<then`, 0 otherwise.
+"""
+function experimental_is_lt(n::Number, then::Number, args...)
+    return Int(n < then)
+end
+
+"""
+    experimental_not(n::Number, args...)
+
+Returns 1 if `n<=.5`, 0 otherwise. If applied in the boolean domain negates the argument.
+"""
+function experimental_not(n::Number, args...)
+    # return Int(!Bool(round(Int, clamp(n, 0 ,1))))
+    return Int(n <= .5)
+end
+
+
 # STRING 
 """
 """
@@ -40,5 +69,8 @@ end
 
 append_method!(bundle_integer_cond, is_eq_to)
 append_method!(bundle_integer_cond, str_is_empty)
+append_method!(bundle_integer_cond, experimental_is_gt)
+append_method!(bundle_integer_cond, experimental_is_lt)
+append_method!(bundle_integer_cond, experimental_not)
 end
 
