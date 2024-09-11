9b78bc4a3365e871b9887c22a9a9bb0746e8792a
Camilo De La Torre <camilo.de-la-torre@ut-capitole.fr>
2024-09-11 13:37:07 +0200

    map_elites_pop

--- src/population/mapelites_repertoire.jl
+++ src/population/mapelites_repertoire.jl
@@ -1,12 +1,23 @@
+
 # the MapelitesRepertoire should be created with the centroids ready but not with the pop / fitness_values
-struct MapelitesRepertoire <: AbstractPopulation
+abstract type Abstract_MapElitesRepertoire <: AbstractPopulation end
+
+"""
+"""
+struct MapelitesRepertoire <: Abstract_MapElitesRepertoire
     pop::Vector{Union{UTGenome,Missing}}
     fitness_values::Vector{Union{Number,Missing}}
     descriptors::Vector{Union{Vector{Number},Missing}}
     centroids::Vector{Vector{Number}}
 end
 
-function insert!(rep::MapelitesRepertoire, genome::UTGenome, fitness::Number, descriptors::Vector{Number})
+# API
+function insert!(
+    rep::MapelitesRepertoire,
+    genome::UTGenome,
+    fitness::Number,
+    descriptors::Vector{Number},
+)
     idx = argmin([norm(descriptors .- centroid) for centroid in rep.centroids])
     # check the strategy for replacement: new equal? new smaller? only if better fitness?
     if ismissing(rep.fitness_values[idx]) || fitness <= rep.fitness_values[idx]
@@ -16,14 +27,19 @@ function insert!(rep::MapelitesRepertoire, genome::UTGenome, fitness::Number, de
     end
 end
 
-function batch_insert!(rep::MapelitesRepertoire, genomes::Vector{UTGenome}, fitness_values::Vector{Number}, descriptors::Vector{Vector{Number}})
-    for ind_idx in 1:length(genomes)
+function batch_insert!(
+    rep::MapelitesRepertoire,
+    genomes::Vector{UTGenome},
+    fitness_values::Vector{Number},
+    descriptors::Vector{Vector{Number}},
+)
+    for ind_idx = 1:length(genomes)
         insert!(rep, genomes[ind_idx], fitness_values[ind_idx], descriptors[ind_idx])
     end
 end
 
 function coverage(rep::MapelitesRepertoire)
-    return count(ismissing, rep.fitness_values) / length(rep.pop) 
+    return count(ismissing, rep.fitness_values) / length(rep.pop)
 end
 
 Base.size(rep::MapelitesRepertoire) = length(rep.pop)
