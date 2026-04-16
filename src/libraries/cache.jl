4e9f6eb8160b0a1e5f20b2fb06d66160246de9e8
Camilo <dlt.camilo@gmail.com>
2026-04-16 11:20:09 +0200

    cache error

--- src/libraries/cache.jl
+++ src/libraries/cache.jl
@@ -11,7 +11,7 @@ end
 struct NoCacheConfig <: AbstractCacheConfig end
 
 function _create_fn_cache(cache_config::CacheConfig)
-    return LRU{cache_config.cache_key_type, cache_config.cache_value_type}(cache_config.cache_size)
+    return LRU{cache_config.cache_key_type, cache_config.cache_value_type}(; maxsize = cache_config.cache_size)
 end
 
 _create_fn_cache(cache_config::NoCacheConfig) = nothing
